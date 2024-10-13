7649117a9c7f280b5eefd63573e4e9e8872a2ed9
NoahSaas <151681167+NoahSaas@users.noreply.github.com>
2024-10-13 17:32:00 +0200

    .

--- main.rb
+++ main.rb
@@ -2,21 +2,25 @@ require 'base64'
 require 'openssl'
 require 'chunky_png'
 
+RESOLUTION = [1920, 1080]
 
-def save_key_to_file(key, file_name)
-    File.open(file_name, 'wb') do |file|
-        file.write(key)
+iv = File.read("iv.bin")
+key = File.read("key.txt")
+encrypted_data = File.read("encrypted_data.txt")
+
+
+
+def write_file(data, file_path)
+    File.open(file_path, 'wb') do |file|
+        file.write(data)
     end
 end
 
 
 def data_to_string(file_path)
     file_data = File.binread(file_path)
-    Base64.encode64(file_data)
+    Base64.strict_encode64(file_data)
 end
-
-
-encoded_data = data_to_string("ball.jpg")
 
 
 def encrypt_string(data, key)
@@ -26,46 +30,48 @@ def encrypt_string(data, key)
     iv = cipher.random_iv
     cipher.iv = iv
     encrypted_data = cipher.update(data) + cipher.final
-    { encrypted_data: Base64.encode64(encrypted_data), iv: Base64.encode64(iv) }
+    { encrypted_data: Base64.strict_encode64(encrypted_data), iv: Base64.strict_encode64(iv) }
+    write_file(Base64.strict_encode64(iv), "iv.bin")
+    write_file(Base64.strict_encode64(key), "key.txt")
+    write_file(Base64.strict_encode64(encrypted_data), "encrypted_data.txt")
 end
-
-
-key = OpenSSL::Cipher.new('AES-256-CBC').random_key
-encrypted_result = encrypt_string(encoded_data, key)
-
-save_key_to_file(key, "key")
 
 
 def string_to_images(encoded_string, chunk_size)
     images = []
     num_chunks = (encoded_string.length / chunk_size.to_f).ceil
-    num_chunks.times do |i|
-        chunk = encoded_string[i * chunk_size, chunk_size] || "" 
-        image = ChunkyPNG::Image.new(1920, 1080, ChunkyPNG::Color::WHITE)
-        chunk.each_char.with_index do |char, index|
-            x = index % 1920
-            y = index.div(1920)
-            break if y >= 1080
-            image[x, y] = ChunkyPNG::Color.rgb(char.ord, char.ord, char.ord)
-        end
-        images << image 
-    end
+    block_size = 6
 
-    images.each_with_index do |image, index|
-        image.save("chunk_#{index}.png")
+    num_chunks.times do |i|
+        chunk = encoded_string[i * chunk_size, chunk_size] || ""
+        image = ChunkyPNG::Image.new(RESOLUTION[0], RESOLUTION[1], ChunkyPNG::Color::WHITE)
+
+        chunk.each_char.with_index do |char, index|
+            block_x = (index * block_size) % RESOLUTION[0]
+            block_y = (index / (RESOLUTION[0] / block_size)) * block_size
+            break if block_y >= RESOLUTION[1]
+
+            color_value = char.ord
+            block_color = ChunkyPNG::Color.rgb(color_value, color_value, color_value)
+
+            block_size.times do |dx|
+                block_size.times do |dy|
+                    x = block_x + dx
+                    y = block_y + dy
+                    image[x, y] = block_color if x < RESOLUTION[0] && y < RESOLUTION[1]
+                end
+            end
+        end
+
+        images << image
+        image.save("chunk_#{i}.png")
     end
 end
-
-
-#string_to_images(encrypted_result[:encrypted_data], 1920 * 1080)  
-
+ 
 
 def create_video_from_images(image_pattern, output_video)
     system("ffmpeg -framerate 24 -i #{image_pattern} -c:v libx264 -pix_fmt yuv420p #{output_video}")
 end
- 
-
-#create_video_from_images("chunk_%d.png", "output_video.mp4")
 
 
 def extract_images_from_video(input_video)
@@ -78,7 +84,7 @@ def images_to_string(images)
     images.each do |image_path|
         image = ChunkyPNG::Image.from_file(image_path)
         image.pixels.each do |pixel|
-            combined_string << (pixel >> 16 & 0xFF).chr  # Assuming pixel values are stored in RGB format
+            combined_string << (pixel >> 16 & 0xFF).chr
         end
     end
     combined_string
@@ -88,10 +94,17 @@ end
 def decrypt_string(encrypted_data, key, iv)
     decipher = OpenSSL::Cipher.new('AES-256-CBC')
     decipher.decrypt
-    decipher.key = key
-    decipher.iv = Base64.decode64(iv)
-    decrypted_data = decipher.update(Base64.decode64(encrypted_data)) + decipher.final
-    decrypted_data
+    decipher.key = Base64.strict_decode64(key)
+    decipher.iv = Base64.strict_decode64(iv)
+    decrypted_data = decipher.update(Base64.strict_decode64(encrypted_data)) + decipher.final
+    binary_file = Base64.strict_decode64(decrypted_data)
+end
+
+
+def generate_data(file_name)
+    b64_string = data_to_string(file_name)
+    key = OpenSSL::Cipher.new('AES-256-CBC').random_key
+    encoded_string = encrypt_string(b64_string, key)
 end
 
 
